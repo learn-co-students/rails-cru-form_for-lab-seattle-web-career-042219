64e1515b0bd3a9c8ee5955d53b95496bb019fedb
Sean Watters <sean@watters.io>
2019-05-21 13:31:41 -0700

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,37 @@
+class ArtistsController < ApplicationController
+
+  def index
+    @artists = Artist.all
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.create(artist_params)
+    redirect_to @artist
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    Artist.update(artist_params)
+    redirect_to @artist
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name, :bio)
+  end
+
+
+end
