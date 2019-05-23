6864f9971e8162e070bc103e5a271f2d0d4ec4b5
austenhasty <hastyae@gmail.com>
2019-05-22 21:11:53 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :songs, :genres, :artists, only: [:show, :create, :update, :new, :edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'songs', to: 'songs#index'
 end
